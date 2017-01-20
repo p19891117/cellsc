b648c18ba3c2d9910f5a9ed3aeedd5642c431400
tst <329126089@qq.com>
2017-01-20 16:45:42 +0800

    增加界面

--- client/src/main/java/com/tst/cellsc/ui/LoginUI.java
+++ client/src/main/java/com/tst/cellsc/ui/LoginUI.java
@@ -10,7 +10,6 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.border.EmptyBorder;
 
 public class LoginUI extends JFrame {
 
